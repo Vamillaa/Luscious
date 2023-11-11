d4083ccf943946d23f4e030d4fa419119a522d37
Vamillaa <147577977+Vamillaa@users.noreply.github.com>
2023-11-11 21:09:10 +0700

    Update home.dart

--- screens/home.dart
+++ screens/home.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyHome extends StatefulWidget {
-  const MyHome({super.key});
-
-  @override
-  State<MyHome> createState() => _MyHomeState();
-}
-
-class _MyHomeState extends State<MyHome> {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,96 +8,158 @@ class _MyHomeState extends State<MyHome> {
         title: Text("Luscious"),
         backgroundColor: Colors.brown[100],
       ),
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.brown[100],
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.brown[50],
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search...',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          prefixIcon: Icon(Icons.search),
-                        ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.brown[50],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              height: 50,
+              alignment: Alignment.center,
+              color: Colors.brown, // You can adjust the color
+              child: Text(
+                'Sweetness Today',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              height: 200,
+              color: Colors.white,
+              child: ClipRRect(
+                child: Image.asset(
+                  'images.jpg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              height: 400, // Adjust the height based on your needs
+              color: Colors.brown[50],
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Align text to the left
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, top: 10, right: 10),
+                    child: Text(
+                      'Recent Desert',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.brown, // Adjust color as needed
                       ),
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Container(
-                    child: Column(
+                  SizedBox(
+                      height: 10), // Adjust the space between text and images
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2, // Number of columns in the grid
+                      crossAxisSpacing: 15, // Spacing between columns
+                      mainAxisSpacing: 10, // Spacing between rows
                       children: [
-                        Flexible(
-                          flex: 1,
-                          child: Container(
-                              margin: EdgeInsets.all(5),
-                              width: double.infinity,
-                              height: double.infinity,
-                              alignment: Alignment.center,
-                              child: Text(
-                                'Sweetnes Today',
-                                style: TextStyle(
-                                  fontSize: 50,
-                                  color: Colors.brown,
-                                ),
-                              )),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image1.jpg', fit: BoxFit.cover),
                         ),
-                        Flexible(
-                          flex: 4,
-                          child: Container(
-                            margin: EdgeInsets.all(5),
-                            width: double.infinity,
-                            height: double.infinity,
-                            color: Colors.white,
-                            child: ClipRRect(
-                              child: Image.asset(
-                                'images.jpg',
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image2.jpg', fit: BoxFit.cover),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image3.jpg', fit: BoxFit.cover),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image4.jpg', fit: BoxFit.cover),
                         ),
                       ],
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.brown[50],
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search...',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          prefixIcon: Icon(Icons.search),
-                        ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              height: 400, // Adjust the height based on your needs
+              color: Colors.brown[50],
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Align text to the left
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, top: 10, right: 10),
+                    child: Text(
+                      'Most View',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.brown, // Adjust color as needed
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                      height: 10), // Adjust the space between text and images
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2, // Number of columns in the grid
+                      crossAxisSpacing: 15, // Spacing between columns
+                      mainAxisSpacing: 10, // Spacing between rows
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image1.jpg', fit: BoxFit.cover),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image2.jpg', fit: BoxFit.cover),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image3.jpg', fit: BoxFit.cover),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('image4.jpg', fit: BoxFit.cover),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
       ),
     );
   }
